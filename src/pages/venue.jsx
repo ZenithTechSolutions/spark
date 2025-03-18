55c10496fda2547ae78dbfb68a6651fe67eb3035
Giri prasath <140329858+Giriprasath1726@users.noreply.github.com>
2025-03-18 13:01:01 +0530

    Add files via upload

---
+++ src/pages/venue.jsx
@@ -0,0 +1,31 @@
+import '../styles/venue.css';
+function Venue(){
+    return(
+        <div class="total">
+        <div class="text">
+            <p><span>Spark 2025</span> will take place at the <span>KPR Institute of Engineering and Technology (KPRIET)</span>.</p>
+            <p>The SPARK Conference 2025 will take place at KPR Institute of Engineering and Technology, hosted across state-of-the-art halls, including Ragam Hall, Dhanam Hall, Pallavi Hall, Veena Hall, and Geetham Hall. Each venue is equipped with modern facilities to ensure a seamless and engaging conference experience.</p>
+        </div>
+        <div class="grid-container">
+            <div class="image">
+                <div class="img1">
+                    <img src="./Images/venue1.jpg" alt="Image 1"/>
+                </div>
+                <div class="img2">
+                    <img src="./Images/venue2.jpg" alt="Image 2"/>
+                </div>
+                <div class="img3">
+                    <img src="./Images/venue3.jpg" alt="Image 3"/>
+                </div>
+                <div class="img4">
+                    <img src="./Images/venue4.jpg" alt="Image 4"/>
+                </div>
+                <div class="img5">
+                    <img src="./Images/venue5.jpg" alt="Image 5"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default Venue
