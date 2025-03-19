9efb326c0d58122daa890dd8b1ba859de8cf3331
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-03-19 12:23:28 +0530

    Header Media Query

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,57 +1,84 @@
-import '../styles/header.css'
-import { Link } from 'react-router-dom'
-import logo from '../assets/kpr_main_logo.png'
-
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+import logo from "../assets/kpr_main_logo.png";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
 
 function Header() {
-    return (
-        <>
-            <div className='header'>
-                <div className="header_content">
-                    <p>International Conference on Smart Systems, Power Electronics Artificial Intelligence, Robotics and
-                        Knowledge Engineering(SPARK'25)
-                    </p>
-                    <img src={logo} alt="KPRIET LOGO" />
-                </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setOpenDropdown(null); // Close dropdowns when menu toggles
+  };
+
+  const toggleDropdown = (dropdownName) => {
+    setOpenDropdown(openDropdown === dropdownName ? null : dropdownName);
+  };
+
+  return (
+    <>
+      <div className="header">
+        <div className="header_content">
+          <div className="hamburger" onClick={toggleMenu}>
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </div>
+          <p>
+            International Conference on Smart Systems, Power Electronics
+            Artificial Intelligence, Robotics and Knowledge Engineering
+            (SPARK'25)
+          </p>
+          <img src={logo} alt="KPRIET LOGO" />
+        </div>
+      </div>
+
+      {/* Navbar Section */}
+      <nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li><Link to="/Home" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/About-Us" onClick={toggleMenu}>About Us</Link></li>
+
+          {/* Committee Dropdown */}
+          <li className="dropdown-container" onClick={() => toggleDropdown("committee")}>
+            Committee <span> &#x2B9F;</span>
+            <div className={`dropdown ${openDropdown === "committee" ? "open" : ""}`}>
+              <Link to="/Committees" onClick={toggleMenu}><p>Committees</p></Link>
+              <Link onClick={toggleMenu}><p>Special Technical Committees</p></Link>
             </div>
-            <nav className="navbar">
-                <ul>
-                    <li><Link to='/Home'>Home</Link></li>
-                    <li><Link to='/About-Us'>About Us</Link></li>
-                    <li className='dropdown-container'>
-                        Committee   <span> &#x2B9F;</span>
-                        <div className='dropdown'>
-                            <Link to='/Committees'><p>Committees</p></Link>
-                            <Link ><p>Special Technical Committees</p></Link>
-                        </div>
-                    </li>
-                    <li><Link to='/Guidelines'>Guidelines</Link></li>
-                    <li><Link to='/Call-for-papers'>Call for Papers</Link></li>
-                    <li><Link to='/Speakers'>Speakers</Link></li>
-                    <li className='dropdown-container'>
-                        Attend  <span>&#x2B9F;</span>
-                        <div className="dropdown">
-                            <Link to='/Explore'><p>Explore Tamil Nadu</p></Link>
-                            <Link to='/Venue' ><p>Conference Venue</p></Link>
-                            <Link to='/Accommodation'><p>Accomodation</p></Link>
-                        </div>
-                    </li>
-                    <li className='dropdown-container'>
-                        Registration <span>&#x2B9F;</span>
-                        <div className='dropdown'>
-                            <Link to='https://googleformlink' target='_blank'><p>Register</p></Link>
-                            <Link to='https://drive.google.com/file/d/18rZ194D-BlNOfBFEkEv3s21ybQ2DM7Kz/vi' target='_blank'><p>Download Brochure</p></Link>
-                            <Link ><p>Workshops</p></Link>
-                        </div>
-                    </li>
-                    <li><Link to='/Plagiarism-Policy'>Plagiarism Policy</Link></li>
-                    <li><Link to='/Contact-Us'>Contact Us</Link></li>
-                    <li className='btn'><Link>Submit Paper</Link></li>
-                </ul>
-            </nav>
-        </>
-    );
+          </li>
+
+          <li><Link to="/Guidelines" onClick={toggleMenu}>Guidelines</Link></li>
+          <li><Link to="/Call-for-papers" onClick={toggleMenu}>Call for Papers</Link></li>
+          <li><Link to="/Speakers" onClick={toggleMenu}>Speakers</Link></li>
+
+          {/* Attend Dropdown */}
+          <li className="dropdown-container" onClick={() => toggleDropdown("attend")}>
+            Attend <span>&#x2B9F;</span>
+            <div className={`dropdown ${openDropdown === "attend" ? "open" : ""}`}>
+              <Link to="/Explore" onClick={toggleMenu}><p>Explore Tamil Nadu</p></Link>
+              <Link to="/Venue" onClick={toggleMenu}><p>Conference Venue</p></Link>
+              <Link to="/Accommodation" onClick={toggleMenu}><p>Accommodation</p></Link>
+            </div>
+          </li>
+
+          {/* Registration Dropdown */}
+          <li className="dropdown-container" onClick={() => toggleDropdown("registration")}>
+            Registration <span>&#x2B9F;</span>
+            <div className={`dropdown ${openDropdown === "registration" ? "open" : ""}`}>
+              <Link to="https://googleformlink" target="_blank" onClick={toggleMenu}><p>Register</p></Link>
+              <Link to="https://drive.google.com/file/d/18rZ194D-BlNOfBFEkEv3s21ybQ2DM7Kz/vi" target="_blank" onClick={toggleMenu}><p>Download Brochure</p></Link>
+              <Link onClick={toggleMenu}><p>Workshops</p></Link>
+            </div>
+          </li>
+
+          <li><Link to="/Plagiarism-Policy" onClick={toggleMenu}>Plagiarism Policy</Link></li>
+          <li><Link to="/Contact-Us" onClick={toggleMenu}>Contact Us</Link></li>
+          <li className="btn"><Link onClick={toggleMenu}>Submit Paper</Link></li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
