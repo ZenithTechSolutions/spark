b50a63466bf522f089dfcd40668f84058b30e802
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-03-21 12:03:00 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,8 +6,6 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [openDropdown, setOpenDropdown] = useState(null);
-
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -18,7 +16,7 @@ function Header() {
       <div className="header">
         <div className="header_content">
           <div className="hamburger" onClick={toggleMenu}>
-            { <FaBars />}
+            { isMenuOpen ?  null : <FaBars />}
           </div>
           <p>
             International Conference on Smart Systems, Power Electronics
@@ -39,7 +37,7 @@ function Header() {
           {/* Committee Dropdown */}
           <li className="dropdown-container">
             Committee <span> &#x2B9F;</span>
-            <div className={`dropdown ${openDropdown === "committee" ? "open" : ""}`}>
+            <div className="dropdown">
               <Link to="/Committees" onClick={toggleMenu}><p>Committees</p></Link>
               <Link onClick={toggleMenu}><p>Special Technical Committees</p></Link>
             </div>
@@ -50,9 +48,9 @@ function Header() {
           <li><Link to="/Speakers" onClick={toggleMenu}>Speakers</Link></li>
 
           {/* Attend Dropdown */}
-          <li className="dropdown-container" onClick={() => toggleDropdown("attend")}>
+          <li className="dropdown-container">
             Attend <span>&#x2B9F;</span>
-            <div className={`dropdown ${openDropdown === "attend" ? "open" : ""}`}>
+            <div className="dropdown" >
               <Link to="/Explore" onClick={toggleMenu}><p>Explore Tamil Nadu</p></Link>
               <Link to="/Venue" onClick={toggleMenu}><p>Conference Venue</p></Link>
               <Link to="/Accommodation" onClick={toggleMenu}><p>Accommodation</p></Link>
@@ -60,9 +58,9 @@ function Header() {
           </li>
 
           {/* Registration Dropdown */}
-          <li className="dropdown-container" onClick={() => toggleDropdown("registration")}>
+          <li className="dropdown-container">
             Registration <span>&#x2B9F;</span>
-            <div className={`dropdown ${openDropdown === "registration" ? "open" : ""}`}>
+            <div className="dropdown">
               <Link to="/Register" onClick={toggleMenu}><p>Register</p></Link>
               <Link to="https://drive.google.com/file/d/18rZ194D-BlNOfBFEkEv3s21ybQ2DM7Kz/vi" target="_blank" onClick={toggleMenu}><p>Download Brochure</p></Link>
               <Link onClick={toggleMenu}><p>Workshops</p></Link>
